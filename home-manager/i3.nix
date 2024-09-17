7ae91f076c1f0581e306c8b451c9bdb778092970
Adam Jasinski <adam@jasinski.lt>
2024-09-17 08:53:41 +0300

    add brightness ctrl to antimage, swap escape with caps

--- home-manager/i3.nix
+++ home-manager/i3.nix
@@ -22,6 +22,8 @@
           "${modifier}+Return" = "exec wezterm";
           "${modifier}+Shift+q" = "kill";
           "${modifier}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run";
+	  "XF86MonBrightnessUp" = "exec --no-startup-id brightnessctl set 5%+";
+	  "XF86MonBrightnessDown" = "exec --no-startup-id brightnessctl set 5%-";
         };
 
       window.commands = [
