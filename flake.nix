cb970ecfb86c6efae5eda415485623d5ba9b28f8
Adam Jasinski <adam@jasinski.lt>
2025-12-08 14:08:58 +0200

    upgrade to 25.05

--- flake.nix
+++ flake.nix
@@ -3,20 +3,16 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    #nixvim = {
-    #  url = "github:adezxc/nixvim";
-    #  inputs.nixpkgs.follows = "nixpkgs";
-    #};
     nixarr.url = "github:rasmus-kirk/nixarr";
   };
 
@@ -42,7 +38,6 @@
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
     nixosModules = import ./modules/nixos;
-    homeManagerModules = import ./modules/home-manager;
 
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
